239177c04e64bffb92cd8c1f6761906c4a9addb8
Kritz72 <krittika.mondal2002@gmail.com>
2024-09-27 18:32:36 +0530

    app

--- app.py
+++ app.py
@@ -154,4 +154,5 @@ def search_history():
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
